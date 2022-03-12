b899cea24fd5b3207a74cf0a2519a3aa7f2eadda
kpmrozowski <kpmrozowski@gmail.com>
2022-03-12 17:54:54 +0100

    loading data works (amend)

--- cnn/out/build/_deps/extern_cxxopts-build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/wut-mini-deep-learning/cnn/extern/cxxopts/src
-# Build directory: /workspace/wut-mini-deep-learning/cnn/out/build/_deps/extern_cxxopts-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cnn/out/eden.sh
+++
@@ -1,1 +0,0 @@
-rsync --exclude 'out' --progress -azv -e 'ssh -J mrozowskik@ssh.mini.pw.edu.pl' /workspace/wut-mini-deep-learning kmrozowski@eden.mini.pw.edu.pl:rl2/
