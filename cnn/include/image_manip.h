caa32b50ce283b60f56b95bcd8e7b59cce651660
Mateusz Majewski <m.majewski2@student.mini.pw.edu.pl>
2022-04-03 00:30:56 +0200

    Yeet OpenCV

--- cnn/include/image_manip.h
+++ cnn/include/image_manip.h
@@ -1,11 +1,8 @@
 #pragma once
 
 #include <torch/torch.h>
-#include <opencv2/core.hpp>
 
 namespace image_manip {
-    cv::Mat torch_to_cv(torch::Tensor in);
-    torch::Tensor cv_to_torch(cv::Mat in);
     torch::Tensor flip_x(torch::Tensor in);
     torch::Tensor flip_y(torch::Tensor in);
     torch::Tensor decolor(torch::Tensor in, int channel);
--- cnn/src/image_manip.cpp
+++ cnn/src/image_manip.cpp
@@ -1,33 +1,8 @@
 #include <image_io.h>
 #include <torch/torch.h>
-#include <opencv2/opencv.hpp>
 #include "image_manip.h"
 
 namespace image_manip {
-    // Heavily adapted from https://discuss.pytorch.org/t/libtorch-c-convert-a-tensor-to-cv-mat-single-channel/47701/6
-    // Or at least were in the past, by now I'm not so sure.
-    cv::Mat torch_to_cv(torch::Tensor in) {
-        cv::Mat out(in.sizes()[1], in.sizes()[2], CV_32FC3);
-        for (auto i = 0; i < in.sizes()[1]; ++i) {
-            for (auto j = 0; j < in.sizes()[2]; ++j) {
-                out.at<cv::Vec3f>(cv::Point(i, j)) = cv::Vec3f(in[0][i][j].item<float>(), in[1][i][j].item<float>(), in[2][i][j].item<float>());
-            }
-        }
-        return out;
-    }
-    torch::Tensor cv_to_torch(cv::Mat in) {
-        auto out = at::empty({3, in.cols, in.rows});
-        for (auto i = 0; i < in.cols; ++i) {
-            for (auto j = 0; j < in.rows; ++j) {
-                auto p = in.at<cv::Vec3f>(cv::Point(i, j));
-                out[0][i][j] = p[0];
-                out[1][i][j] = p[1];
-                out[2][i][j] = p[2];
-            }
-        }
-        return out;
-    }
-
     torch::Tensor flip_x(torch::Tensor in) {
         return in.flip({1});
     }
