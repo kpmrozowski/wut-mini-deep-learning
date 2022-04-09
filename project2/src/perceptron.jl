0d5f4196bcc663db77545ec49f0919c8cce01c04
Mateusz Majewski <m.majewski2@student.mini.pw.edu.pl>
2022-04-09 17:33:33 +0200

    Make it runnable on CUDA

--- project2/src/perceptron.jl
+++ project2/src/perceptron.jl
@@ -5,13 +5,10 @@ function perceptron(seed::Integer)
     yes = filter(x -> x.label == :yes, train)
     no = filter(x -> x.label == :no, train)
 
-    x_train = reduce(hcat, rpad.(getfield.(vcat(yes, no), :data), 16000, 0)) |> gpu
-    # x_train = convert.(Float32, reduce(hcat, rpad.(getfield.(vcat(yes, no), :data), 16000, 0))) |> gpu # (1)
-    y_train = onehotbatch(getfield.(vcat(yes, no), :label), [:yes, :no]) |> gpu
-    # y_train = convert.(Float32, onehotbatch(getfield.(vcat(yes, no), :label), [:yes, :no])) |> gpu # (2)
+    x_train = convert.(Float32, reduce(hcat, rpad.(getfield.(vcat(yes, no), :data), 16000, 0))) |> gpu
+    y_train = convert.(Float32, onehotbatch(getfield.(vcat(yes, no), :label), [:yes, :no])) |> gpu
 
     model = Chain(
-        x -> convert.(Float32, x ./ 2^15), # (3)
         Dense(16000 => 800, relu, init=glorot_uniform(rng)),
         Dense(800 => 40, relu, init=glorot_uniform(rng)),
         Dense(40 => 2, init=glorot_uniform(rng))
