3d04d38c7b4bf7529ef43ee358e8a4bfb3e37ef7
kpmrozowski <kpmrozowski@gmail.com>
2022-05-01 00:24:22 +0200

    cnn-rnn-lstm added

--- speech-recognition/models.py
+++ speech-recognition/models.py
@@ -88,6 +88,89 @@ class ResNet():
         
         self.m = Model(i,x)
         return self.m
+#%%
+class ResNetLstm():
+    """
+    Usage: 
+        sr = ResNetLstm([4,8,16], input_size=(50,50,1), output_size=12)
+        sr.build()
+        followed by sr.m.compile(loss='categorical_crossentropy', 
+                                 optimizer='adadelta', metrics=["accuracy"])
+        save plotted model with: 
+            keras.utils.plot_model(sr.m, to_file = '<location>.png', 
+                                   show_shapes=True)
+    """
+    def __init__(self,
+                 filters_list=[], 
+                 input_size=None, 
+                 output_size=None,
+                 initializer='glorot_uniform'):
+        self.filters_list = filters_list
+        self.input_size = input_size
+        self.output_size = output_size
+        self.initializer = initializer
+        self.m = None        
+    
+    def _block(self, filters, inp):
+        """ one residual block in a ResNetLstm
+        
+        Args:
+            filters (int): number of convolutional filters
+            inp (tf.tensor): output from previous layer
+            
+        Returns:
+            tf.tensor: output of residual block
+        """
+        layer_1 = BatchNormalization()(inp)
+        act_1 = Activation('relu')(layer_1)
+        conv_1 = Conv2D(filters, (3,3), 
+                        padding = 'same', 
+                        kernel_initializer = self.initializer)(act_1)
+        layer_2 = BatchNormalization()(conv_1)
+        act_2 = Activation('relu')(layer_2)
+        conv_2 = Conv2D(filters, (3,3), 
+                        padding = 'same', 
+                        kernel_initializer = self.initializer)(act_2)
+        return(conv_2)
+
+    def build(self):
+        """
+        Returns:
+            keras.engine.training.Model
+        """
+        i = Input(shape = self.input_size, name = 'input')
+        x = Conv2D(self.filters_list[0], (3,3), 
+                   padding = 'same', 
+                   kernel_initializer = self.initializer)(i)
+        x = MaxPooling2D(padding = 'same')(x)        
+        x = Add()([self._block(self.filters_list[0], x),x])
+        x = Add()([self._block(self.filters_list[0], x),x])
+        x = Add()([self._block(self.filters_list[0], x),x])
+        if len(self.filters_list) > 1:
+            for filt in self.filters_list[1:]:
+                x = Conv2D(filt, (3,3),
+                           strides = (2,2),
+                           padding = 'same',
+                           activation = 'relu',
+                           kernel_initializer = self.initializer)(x)
+                x = Add()([self._block(filt, x),x])
+                x = Add()([self._block(filt, x),x])
+                x = Add()([self._block(filt, x),x])
+        x = GlobalAveragePooling2D()(x)
+        
+        print('shapes: ', x.shape, int(x.shape[-1]) * int(x.shape[-2]))
+        x = Reshape((16,9*128))(x)
+        x = Bidirectional(CuDNNLSTM(128,return_sequences=True))(x)
+        x = RepeatVector(Dense(128))(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = Bidirectional(CuDNNLSTM(128,return_sequences=True))(x)
+        x = Reshape((16,9*128))(x)
+        
+        x = Dense(self.output_size, activation = 'softmax')(x)
+        
+        self.m = Model(i,x)
+        return self.m
            
     
 #%%
