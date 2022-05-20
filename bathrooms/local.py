a121e560981d534c82d730968ef38ba16818be65
Mateusz Majewski <metiulekm@gmail.com>
2022-05-20 21:12:00 +0200

    Add basic DCGAN

---
+++ bathrooms/local.py
@@ -0,0 +1,12 @@
+# Feel free to put local changes here.
+
+# Paths
+import os
+dataset_path = os.path.dirname(__file__) + "/dataset"
+
+# Local parameters
+batch_size = 64
+workers = 2
+seed = 69
+num_epochs = 10
+ngpu = 0
