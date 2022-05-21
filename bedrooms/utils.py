f5a4f1fdf871e05cc6cb50bfcde9ca78d5d537ba
kpmrozowski <kpmrozowski@gmail.com>
2022-05-21 19:15:01 +0200

    more precise printing when training

--- bedrooms/utils.py
+++ bedrooms/utils.py
@@ -26,7 +26,7 @@ class SaveBestModel:
     ):
         if current_valid_loss < self.best_valid_loss:
             self.best_valid_loss = current_valid_loss
-            print(f"\tEpoch {epoch+1}, saving best model, loss: {self.best_valid_loss}")
+            print(f'\tEpoch {epoch+1}, saving best' + self.name + 'model, loss: {self.best_valid_loss}')
             torch.save({
                 'epoch': epoch+1,
                 'model_state_dict': model.state_dict(),
